db4dabfc81d24a6ee93f3edd00bde6813ae1d32d
kushith <kushrgo1935@gmail.com>
2026-03-09 19:39:48 +0530

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Skills from './pages/Skills'
-import Projects from './pages/Projects'
-import Achievements from './pages/Achievements'
-import Contact from './pages/Contact'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Achievements from "./pages/Achievements";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -17,6 +17,6 @@ export default function App() {
         <Route path="/achievements" element={<Achievements />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
